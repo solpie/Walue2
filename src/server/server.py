988161acc825b44953f9b9a9ef22b58cc2fc7cfa
solpie <solpie.net@gmail.com>
2016-11-28 12:34:43 +0800

    pack one file

--- src/server/server.py
+++ src/server/server.py
@@ -2,6 +2,8 @@ import os
 # print(os.path.realpath(__file__))
 # server_path = os.path.realpath(__file__)
 server_path = os.path.dirname(__file__)
+if server_path =="":
+    server_path = os.path.realpath(".")
 print("server root:", server_path)
 # config
 import configparser
@@ -16,12 +18,15 @@ from bottle import get, run, static_file, request, post
 
 @get('/')
 def index():
+    print("get index")
     return 'running'
 
+static_path = os.path.join(server_path, 'static')
+print("static path:", static_path)
 
 @get('/static/<filepath:path>')
 def server_static(filepath):
     print(filepath)
-    return static_file(filepath, root=os.path.join(server_path, 'static'))
+    return static_file(filepath, root=static_path)
 
 run(host='0.0.0.0', port=server_port, reloader=True)
